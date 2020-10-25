baa02ac51b29c0ade60852380229ae75f23428c3
ghyathCode <gheath.farkh@gmail.com>
2020-10-25 08:55:28 +0300

    Test the first case given in the examples

--- solutionTest.py
+++ solutionTest.py
@@ -3,6 +3,10 @@ from solution import *
 
 
 class AnagramSolutionTest(unittest.TestCase):
+    def test_two_strings_are_not_anagram_case_1(self):
+        result = anagram_example_result("Tom Marvolo Riddle", "I Am Lord Voldemort")
+        self.assertEquals(result,
+                          "remove 7 characters from 'Tom Marvolo Riddle' and 8 characters from 'I Am Lord Voldemort'")
 
     def test_two_strings_are_anagram(self):
         result = anagram_example_result("tom marvolo riddle", "i am lordvoldemort")
